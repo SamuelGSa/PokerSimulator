224382a01813ae1ee66368d8b7a96f5bb22ac015
SamuelGSa <sgmsa@icloud.com>
2023-09-21 22:34:23 -0300

    d

--- src/main/java/com/estudo/pokersimulator/PokerSimulatorApplication.java
+++ src/main/java/com/estudo/pokersimulator/PokerSimulatorApplication.java
@@ -4,6 +4,34 @@ public class PokerSimulatorApplication {
 
 	public static void main(String[] args) {
 
+		// Inicialize o jogo e os jogadores
+
+//		JogoDePoker jogo = new JogoDePoker();
+
+
+
+
+//		Jogador jogador1 = new Jogador("Jogador1");
+//		Jogador bot1 = new Jogador("Bot1");
+//
+//		// Adicione os jogadores ao jogo
+//		jogo.adicionarJogador(jogador1);
+//		jogo.adicionarJogador(bot1);
+//
+//		// Inicie o jogo e implemente a lógica do jogo aqui
+//		while (!jogo.terminou()) {
+//			jogo.iniciarRodada();
+//
+//			// Implemente as ações dos jogadores (apostas, descartes, etc.)
+//			jogador1.fazerAcao();
+//			bot1.fazerAcao();
+//
+//			// Continue até que a rodada termine
+//		}
+//
+//		// Exiba o resultado final do jogo
+//		jogo.exibirResultado();
+
 	}
 
 
