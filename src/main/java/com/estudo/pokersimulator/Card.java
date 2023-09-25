603bb09448129e27f50f6168e60a6743a5afd8df
SamuelGSa <sgmsa@icloud.com>
2023-09-25 17:31:30 -0300

    :sparkles: feat: cria metodo que gera um baralho de 52 cartas e 4 naipes

---
+++ src/main/java/com/estudo/pokersimulator/Card.java
@@ -0,0 +1,15 @@
+package com.estudo.pokersimulator;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class Card {
+
+    private String name;
+
+    private  String suit;
+}
